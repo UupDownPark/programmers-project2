6497c20b0fc836b0ee8e7b4a0e75400a215a3a2d
UupDownPark <geakura123@gmail.com>
2024-01-06 13:57:36 +0900

    history fix

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  join,
+  login,
+  requestPasswordReset,
+  passwordReset,
+} = require("../controller/UserController");
+router.use(express.json());
+
+router.post("/join", join);
+
+router.post("/login", login);
+
+router.post("/reset", requestPasswordReset);
+
+router.put("/reset", passwordReset);
+
+module.exports = router;
