aa3373353b7a69ee8c6391e07dc61f23d2c6cddb
UupDownPark <geakura123@gmail.com>
2024-01-29 01:37:52 +0900

    likes finish

--- controller/BookController.js
+++ controller/BookController.js
@@ -6,7 +6,8 @@ const allBooks = (req, res) => {
 
   let offset = limit * (currentPage - 1);
   limit = parseInt(limit);
-  let sql = "SELECT * FROM books";
+  let sql =
+    "SELECT *,(SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes FROM books";
   let values = [];
   if (category_id && news) {
     sql +=
@@ -20,8 +21,8 @@ const allBooks = (req, res) => {
       " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()";
   }
   sql += " LIMIT ?,?";
-  console.log(values);
-  // values.push(offset, limit);
+
+  values.push(offset, limit);
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
@@ -33,12 +34,18 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-  let { id } = req.params;
-  id = parseInt(id);
-  let sql = `select * from books LEFT JOIN category 
-              ON books.category_id = category.id WHERE books.id=?`;
+  let { user_id } = req.body;
+  let book_id = req.params.id;
 
-  conn.query(sql, id, (err, results) => {
+  let sql = `SELECT *,
+            (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
+            (SELECT EXISTS(SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked 
+            FROM books
+            LEFT JOIN category
+            ON books.category_id = category.category_id
+            WHERE books.id=?`;
+  let values = [user_id, book_id, book_id];
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
