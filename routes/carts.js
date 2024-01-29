b0d9c442d8ab74224cb825c0d74040bb8545a8ea
UupDownPark <geakura123@gmail.com>
2024-01-29 15:28:48 +0900

    cart end

--- routes/carts.js
+++ routes/carts.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+} = require("../controller/CartController");
 
 router.use(express.json());
 
-router.post("/", (req, res) => {
-  res.json("장바구니 담기");
-});
+router.post("/", addToCart);
 
-router.get("/", (req, res) => {
-  res.json("장바구니 조회");
-});
+router.get("/", getCartItems);
 
-router.get("/:id", (req, res) => {
-  res.json("카테고리별 도서 목록 조회");
-});
+router.delete("/:id", removeCartItem);
+
+router.get("/", getCartItems);
+
 // //장바구니에서 예상한 상품 조회
 // router.get("/carts", (req, res) => {
 //   res.json("장바구니 조회");
